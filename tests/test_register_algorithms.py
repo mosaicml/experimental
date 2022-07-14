b2a359a8d9af3763784c8841913ff2bf9616ca51
Hanlin Tang <hanlin@mosaicml.com>
2022-07-14 07:56:08 -0700

    maybe fix pyright

--- tests/test_register_algorithms.py
+++ tests/test_register_algorithms.py
@@ -7,6 +7,7 @@ def test_register_algorithms():
     algo_names = experimental.algorithms.register_all_algorithms()
 
     assert len(algo_names) > 0
+    assert TrainerHparams.hparams_registry is not None
 
     for algo in algo_names:
         assert algo in TrainerHparams.hparams_registry["algorithms"]
